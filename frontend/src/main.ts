e2f8f49bbfb4b04572b163ef7001118f4d1802c4
Roman <blackwanted@yandex.ru>
2022-06-12 12:55:10 +0300

    Fix -- Start

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,11 @@ import { createPinia } from "pinia";
 import "./style.scss";
 import App from "./App.vue";
 import router from "./router";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import duration from "dayjs/plugin/duration";
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
 
 const app = createApp(App);
 
--- frontend/src/page/Main.vue
+++ frontend/src/page/Main.vue
@@ -1,6 +1,15 @@
 <template>
   <div :class="$style.main">
-    <Block> Hello </Block>
+    <Block>
+      <div v-if="!isChange" :class="$style.time">12:30:43</div>
+      <Input v-if="isChange" v-model="time" />
+
+      <Row size="2" style="margin-top: 10px">
+        <Button v-if="!isChange" @click="isChange = !isChange" text="Change" />
+        <Button v-if="isChange" @click="sas" text="Ok" />
+        <Button text="Stop" />
+      </Row>
+    </Block>
   </div>
 </template>
 
@@ -11,11 +20,19 @@ import {
   Toggle,
   Input,
   Block,
+  Row,
 } from "../gam-lib-ui/vue/component/ui";
 import { ref } from "vue";
+import dayjs from "dayjs";
+import { TimeHelper } from "@/gam-lib-ui/util/TimeHelper";
 
-const sas = ref(false);
-const sasx = ref("1");
+const time = ref("00:00:00");
+const isChange = ref(false);
+
+function sas() {
+  isChange.value = false;
+  console.log(TimeHelper.HMStoSeconds(time.value));
+}
 </script>
 
 <style module lang="scss">
@@ -26,5 +43,10 @@ const sasx = ref("1");
   align-items: center;
   justify-content: center;
   height: 100%;
+
+  .time {
+    font-size: 24px;
+    text-align: center;
+  }
 }
 </style>
